1ac98846a2f2dbe4d90033f7d93348ea6e39967e
sandervspl <sandervispoel@hotmail.com>
2022-02-08 00:05:19 +0100

    Add watch modes, update build script for server

--- apps/server/esbuild.config.js
+++ apps/server/esbuild.config.js
@@ -1,7 +1,48 @@
-require('esbuild').buildSync({
-  entryPoints: ['index.ts'],
-  outfile: 'dist/index.js',
-  bundle: true,
-  platform: 'node',
-  target: ['node14'],
-});
+const path = require('path');
+const { fork } = require('child_process');
+
+const DEV = process.env.NODE_ENV === 'development';
+
+// Fork process to start/kill app with
+let nodeFork;
+
+function onBuildComplete() {
+  if (DEV) {
+    require('node-notifier').notify({
+      title: 'Monorepo Server',
+      message: 'Build complete!',
+    });
+
+    // Run app with node
+    nodeFork?.kill();
+    nodeFork = fork(path.resolve(__dirname, 'dist/index.js'));
+  }
+}
+
+function build() {
+  require('esbuild').build({
+    entryPoints: ['index.ts'],
+    outfile: 'dist/index.js',
+    bundle: true,
+    platform: 'node',
+    target: ['node14'],
+    external: Object.keys(require('./package.json').dependencies),
+    watch: DEV && {
+      onRebuild(err) {
+        if (err) {
+          console.error('watch build failed:', err);
+          process.exit(1);
+        }
+
+        console.info('🚀 Rebuild complete!\n');
+        onBuildComplete();
+      },
+    },
+  })
+    .catch(() => process.exit(1));
+
+  console.info('🚀 Build complete!\n');
+  onBuildComplete();
+}
+
+build();
